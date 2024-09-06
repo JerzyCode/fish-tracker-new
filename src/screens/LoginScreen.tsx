9673e0a45656a91efd30eb6b6744b59bb6c766c0
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-09-06 20:48:15 +0200

    login screen gui

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,25 +1,123 @@
-import React from "react";
+import React, {useState} from "react";
 import LinearGradient from "react-native-linear-gradient";
-import {bgGradientColors, blueColor, globalStyles} from "../GlobalStyles.tsx";
-import {Text} from "react-native";
+import {blueGradient, darkBlueColor, globalStyles} from "../GlobalStyles.tsx";
+import {ActivityIndicator, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import BackButton from "../components/BackButton.tsx";
+import {useTranslation} from "react-i18next";
+import {useInfoBar} from "../contexts/InfoBarContext.tsx";
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
 
+
+function LoadingFragment(): React.JSX.Element {
+    return (
+        <View style={globalStyles.rlFormInputView}>
+            <ActivityIndicator size="large" animating={true}/>
+        </View>
+    )
+}
 
 function LoginScreen({navigation}: any): React.JSX.Element {
 
+    const {t} = useTranslation()
+    const {dispatch} = useInfoBar();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    const showSuccessInfoBar = (message: string) => {
+        dispatch({
+            type: 'SHOW_INFO_BAR',
+            payload: {
+                message: message,
+                type: 'success',
+                visible: true,
+                duration: 3000
+            }
+        });
+    };
+
+    const showErrorInfoBar = (message: string) => {
+        dispatch({
+            type: 'SHOW_INFO_BAR',
+            payload: {
+                message: message,
+                type: 'error',
+                visible: true,
+                duration: 3000
+            }
+        });
+    }
+
+
+    const resetInputs = () => {
+        setEmail('')
+        setPassword('')
+    }
+
+
+    const onRegisterButton = () => {
+
+
+    }
+
+
     return (
-        <LinearGradient
-            colors={bgGradientColors}
-            style={globalStyles.container}>
-            <BackButton navigation={navigation} color={blueColor}/>
-            <Text>
-                LOGIN SCREEN
-            </Text>
+        <KeyboardAwareScrollView style={{flex: 1}}
+                                 contentContainerStyle={styles.contentContainer}
+                                 keyboardShouldPersistTaps="handled">
+            <View style={styles.inner}>
+                <BackButton navigation={navigation} color={darkBlueColor}/>
+                <LinearGradient
+                    colors={blueGradient}
+                    style={styles.contentContainer}>
 
+                    <View style={globalStyles.rlFormInputHeaderView}>
+                        <Text style={globalStyles.rlFormInputHeaderText}>{t('login-screen.login')}</Text>
+                    </View>
 
-        </LinearGradient>
+                    {isLoading ? <LoadingFragment/> :
+                        <View style={globalStyles.rlFormInputView}>
+                            <Text style={globalStyles.rlFormEnterInputText}>{t('login-screen.enter-inputs')}</Text>
 
+                            <TextInput
+                                style={[globalStyles.rlFormInput]}
+                                placeholder={t('login-screen.email')}
+                                value={email}
+                                onChangeText={setEmail}
+                                keyboardType="email-address"
+                                autoCapitalize="none"
+                            />
+
+                            <TextInput
+                                style={[globalStyles.rlFormInput]}
+                                placeholder={t('login-screen.password')}
+                                value={password}
+                                onChangeText={setPassword}
+                                secureTextEntry
+                            />
+
+                            <TouchableOpacity style={globalStyles.rlFormInputActionBtn} onPress={onRegisterButton}>
+                                <Text
+                                    style={globalStyles.rlFormInputActionBtnText}>{t('login-screen.login')}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    }
+                </LinearGradient>
+            </View>
+        </KeyboardAwareScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    inner: {
+        flex: 1,
+        justifyContent: 'space-around',
+    },
+    contentContainer: {
+        flexGrow: 1,
+        width: '100%',
+    }
+})
 
 export default LoginScreen
